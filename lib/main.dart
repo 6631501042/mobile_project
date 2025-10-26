120e4b84fddbd15855665506a4c88f67e1e2f4e7
6631501042 <6631501042@lamduan.mfu.ac.th>
2025-10-26 22:54:09 +0700

    change file name and mix 3 or 4 page into 1 file because use tabbar.

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,13 @@ import 'package:flutter/material.dart';
 // import 'package:mobile_project/user/register.dart'; // Register
 
 // staff
-import 'package:mobile_project/staff/dashboard_staff.dart'; // DashboardStaff
-// import 'package:mobile_project/staff/dashboard_staff_example.dart'; // DashboardStaff
+// import 'package:mobile_project/staff/dashboard_staff.dart'; // DashboardStaff
+// import 'package:mobile_project/staff/staff.dart'; // Staff
 
 // approver
 // import 'package:mobile_project/approver/dashboard_approver.dart'; // DashboardApprover
-
-// import 'package:mobile_project/gridcard_example.dart';
+import 'package:mobile_project/approver/approver.dart'; // Approver
 
 void main() {
-  runApp(MaterialApp(home: DashboardStaff(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Approver(), debugShowCheckedModeBanner: false));
 }
