8b7c3fe5c51f62c558d89d947357cb089f0465d0
6631501042 <6631501042@lamduan.mfu.ac.th>
2025-10-28 15:55:37 +0700

    delete status.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:mobile_project/user/user.dart'; // User
-// import 'package:mobile_project/staff/staff.dart'; // Staff
+import 'package:mobile_project/staff/staff.dart'; // Staff
 // import 'package:mobile_project/approver/approver.dart'; // Approver
-import 'package:mobile_project/user/status.dart'; // StatusPage
 
 void main() {
-  runApp(MaterialApp(home: StatusPage(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Staff(), debugShowCheckedModeBanner: false));
 }
