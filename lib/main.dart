0fc7eee44454da417f6a489944b571424a62e0d7
Chirat44 <6631501024@lamduan.mfu.ac.th.>
2025-10-28 15:09:58 +0700

    status user ใช้ได้จริง

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
-import 'user/status.dart'; // หรือ package import ให้ตรงชื่อโปรเจ็กต์
+import 'package:mobile_project/user/test.dart'; // Status
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Room Reservation',
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.hampton,
-        primaryColor: AppColors.finlandia,
-      ),
-      // ใช้คลาสที่มีอยู่จริงใน status.dart
-      home: const ApproverStatusPage(
-        approverName: 'Ajarn.Tick',
-        approverId: 'teacher-001',
-      ),
-    );
-  }
+void main() {
+  runApp(MaterialApp(home: Status(), debugShowCheckedModeBanner: false));
 }
