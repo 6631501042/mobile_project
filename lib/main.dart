3822a0fae4b12ffa1cd106611867bf521a322df0
6631501042 <6631501042@lamduan.mfu.ac.th>
2025-10-30 08:44:15 +0700

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 // import 'package:mobile_project/user/login.dart'; // Login
 // import 'package:mobile_project/user/user.dart'; // User
-// import 'package:mobile_project/staff/staff.dart'; // Staff
-import 'package:mobile_project/approver/approver.dart'; // Approver
+import 'package:mobile_project/staff/staff.dart'; // Staff
+// import 'package:mobile_project/approver/approver.dart'; // Approver
 
 void main() {
-  runApp(MaterialApp(home: Approver(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Staff(), debugShowCheckedModeBanner: false));
 }
