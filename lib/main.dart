f1a7208757a6948475a3c62fd69b0bade2bd6b87
6631501051 <6631501051@lamduan.mfu.ac.th>
2025-11-03 14:29:45 +0700

    history

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_project/user/login.dart';
+import 'package:mobile_project/approver/history_approver.dart';
 
 void main() {
-  runApp(MaterialApp(home: Login(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: HistoryApproverPage(), debugShowCheckedModeBanner: false));
 }
